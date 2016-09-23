2941628bb9d26e67804aacf87a58362ee220279d
Gilson Rocha Silva <gilson.silva@cinbesa.pmb.pa.gov.br>
2016-09-22 21:58:08 -0300

    Refatoração para remover o SubContas

--- app/scripts/controllers/ContaCorrenteController.js
+++ app/scripts/controllers/ContaCorrenteController.js
@@ -8,19 +8,10 @@ angular.module('controleFinanceiroApp')
   $scope.contaCorrenteSelecionada = null;
   $scope.contaCorrente = {};
   $scope.contasCorrentes = [];
-  $scope.subcontas = [];
-  $scope.subconta = {};
-  $scope.subcontaSelecionada = null;
 
   $scope.listarContasCorrentes = function() {
     var contaCorrenteResources = $injector.get('ContaCorrenteResources');
-
-    $scope.contasCorrentes = contaCorrenteResources.query(function(result) {
-      if($scope.contasCorrentes.itens.length > 0) {
-        $scope.contaCorrenteSelecionada = $scope.contasCorrentes.itens[0];
-        $scope.listarSubContas($scope.contaCorrenteSelecionada);
-      }
-    });
+    $scope.contasCorrentes = contaCorrenteResources.query();
   }
 
   $scope.selecionarContaCorrente = function(contaCorrente) {
@@ -58,13 +49,6 @@ angular.module('controleFinanceiroApp')
     $scope.contaCorrente.banco = bancoSelecionado;
   }
 
-  $scope.visualizarContaCorrente = function(idContaCorrente) {
-    var contaCorrenteResources = $injector.get('ContaCorrenteResources');
-    $scope.contaCorrenteVisualizar = contaCorrenteResources.get({ id: idContaCorrente });
-
-    $('#modalContaCorrenteVisualizar').modal('show');
-  }
-
   $scope.listarBancos = function() {
     var bancoResources = $injector.get('BancoResources');
     $scope.bancos = bancoResources.query();
@@ -73,45 +57,6 @@ angular.module('controleFinanceiroApp')
   $('#modalContaCorrente').on('shown.bs.modal', function () {
     $scope.listarBancos();
   });
-
-  $('#modalContaCorrenteVisualizar').on('shown.bs.modal', function () {
-    $scope.contaCorrenteVisualizar = null;
-  });
-
-  $scope.listarSubContas = function(contaCorrente) {
-    var subContaResources = $injector.get('SubContaResources');
-    $scope.subcontas = subContaResources.query({idContaCorrente : contaCorrente.id});
-  }
-
-  $scope.adicionarSubConta = function() {
-    $scope.subconta = {};
-    $scope.subconta.contaCorrente = $scope.contaCorrenteSelecionada;
-    $('#modalSubConta').modal('show');
-  }
-
-  $scope.salvarSubConta = function() {
-    var subContaResources = $injector.get('SubContaResources');
-
-    if($scope.subconta.id) {
-      subContaResources.update({
-          idContaCorrente : $scope.subconta.contaCorrente.id,
-          idSubConta : $scope.subconta.id
-        }, $scope.subconta, function(result) {
-          $('#modalSubConta').modal('hide');
-          $scope.listarSubContas($scope.subconta.contaCorrente);
-      });
-    } else {
-      subContaResources.save({idContaCorrente : $scope.subconta.contaCorrente.id}, $scope.subconta, function(result) {
-        $('#modalSubConta').modal('hide');
-        $scope.listarSubContas($scope.subconta.contaCorrente);
-      });
-    }
-  }
-
-  $scope.editarSubConta = function(subconta) {
-    $scope.subconta = angular.copy(subconta);
-    $('#modalSubConta').modal('show');
-  }
 
   $scope.listarContasCorrentes();
 }]);
